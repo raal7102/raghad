d2ab2fd2bbaa95bf52bc4ca8a0169fec4a1e3f97
raal7102 <31807179+raal7102@users.noreply.github.com>
2017-09-09 23:15:42 -0700

    Add files via upload
    first python homework file

---
+++ homework1.py
@@ -0,0 +1,126 @@
+
+# coding: utf-8
+
+# In[1]:
+
+x=5
+y=10
+print x,y
+
+
+# In[2]:
+
+x=10
+y=20
+z=x+y
+print z
+
+
+# In[3]:
+
+a=5
+b=6
+print a+b
+
+
+# In[21]:
+
+greeting = "Hello "
+person = "Oscar"
+personalGreeting = greeting + person
+print personalGreeting
+
+
+# In[22]:
+
+person = int(input('Enter your age: '))
+
+
+# In[23]:
+
+age = int(input('Enter Your Age: '))
+
+
+# In[24]:
+
+if age > 35:
+    print "The age is greater than 35"
+else:
+    print "The age is not greater than 35"
+
+
+# In[25]:
+
+number =int(10)
+number/4
+
+
+# In[26]:
+
+number//4
+
+
+# In[27]:
+
+number%4
+
+
+# In[28]:
+
+newNumber= number%4
+print newNumber
+
+
+# In[29]:
+
+# first division of 10/4 equals 2
+# int division of 10/4 equals 2
+# modulus of 10 by 4 is 2 (remainder of  10/4)
+
+
+# In[30]:
+
+number = 101.35
+number/4
+
+
+# In[31]:
+
+number//4
+
+
+# In[32]:
+
+number%4
+
+
+# In[33]:
+
+newNumber= number%4
+print newNumber
+
+
+# In[34]:
+
+myAge = int(21)
+yourAge = int(25)
+myAge==yourAge
+
+
+# In[36]:
+
+result = myAge==yourAge
+print result
+
+
+# In[38]:
+
+hisAge = int(25)
+results = myAge==hisAge
+print result
+
+
+# In[ ]:
+
+
+
