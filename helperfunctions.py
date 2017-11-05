76295b54dba8db6466e52620baef47d1836c5ed7
raal7102 <31807179+raal7102@users.noreply.github.com>
2017-11-04 22:16:46 -0700

    homewor8a upload with code

---
+++ helperfunctions.py
@@ -0,0 +1,22 @@
+
+
+def add(x,y):
+    return x+y
+
+def diff(x,y):
+    if x >= y:
+        result = x-y
+    else:
+        result = y-x
+    return result
+
+def product(x,y):
+    return x*y
+
+def greatest(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+    
+
